12f3917879a1df6f661cdf2cc7edeada0b3281d6
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-06 21:51:34 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,10 @@
+# # -------------------PYTHON-------------------
+
+# Python Variables
+# A variable is the name given to a memory location. A value-holding Python variable is also known as an identifier.
+
+# Since Python is an infer language that is smart enough to determine the type of a variable, we do not need to specify its type in Python.
+
+# Variable names must begin with a letter or an underscore, but they can be a group of both letters and digits.
+
+# The name of the variable should be written in lowercase. Both Rahul and rahul are distinct variables.
