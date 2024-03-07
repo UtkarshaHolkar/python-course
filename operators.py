f9eee113a943ee82f16da65d22d930db28c3510b
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-07 22:39:43 +0530

    first commit

--- operators.py
+++ operators.py
@@ -1,10 +1,12 @@
- Python also has some operators, and these are given below -
+#  Python also has some operators, and these are given below -
 
-Arithmetic operators
-Comparison operators
-Assignment Operators
-Logical Operators
-Bitwise Operators
-Membership Operators
-Identity Operators
-Arithmetic Operators
+# Arithmetic operators
+# Comparison operators
+# Assignment Operators
+# Logical Operators
+# Bitwise Operators
+# Membership Operators
+# Identity Operators
+# Arithmetic Operators
+
+# Arithmetic operators used between two operands for a particular operation. There are many arithmetic operators. It includes the exponent (**) operator as well as the + (addition), - (subtraction), * (multiplication), / (divide), % (reminder), and // (floor division) operators.
