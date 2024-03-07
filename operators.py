0da7d330960a9a2789e82270df6911aaaf3d153e
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-07 22:38:35 +0530

    first commit

---
+++ operators.py
@@ -0,0 +1,10 @@
+ Python also has some operators, and these are given below -
+
+Arithmetic operators
+Comparison operators
+Assignment Operators
+Logical Operators
+Bitwise Operators
+Membership Operators
+Identity Operators
+Arithmetic Operators
