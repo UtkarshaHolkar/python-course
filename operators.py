d2774fe70f9c3f2c0fc7775351c036b84eea0174
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-14 22:33:08 +0530

    first commit

--- operators.py
+++ operators.py
@@ -10,3 +10,6 @@
 # Arithmetic Operators
 
 # Arithmetic operators used between two operands for a particular operation. There are many arithmetic operators. It includes the exponent (**) operator as well as the + (addition), - (subtraction), * (multiplication), / (divide), % (reminder), and // (floor division) operators.
+
+#Comparison operator
+# Comparison operators mainly use for comparison purposes. Comparison operators compare the values of the two operands and return a true or false Boolean value in accordance. The example of comparison operators are ==, !=, <=, >=, >, <. In the below table, we explain the works of the operators.
