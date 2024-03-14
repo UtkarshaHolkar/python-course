641e536d16f087c6a1939edf9ed9c689ce996897
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-14 22:34:12 +0530

    first commit

--- operators.py
+++ operators.py
@@ -13,3 +13,23 @@
 
 #Comparison operator
 # Comparison operators mainly use for comparison purposes. Comparison operators compare the values of the two operands and return a true or false Boolean value in accordance. The example of comparison operators are ==, !=, <=, >=, >, <. In the below table, we explain the works of the operators.
+a = 32      # Initialize the value of a  
+b = 6       # Initialize the value of b  
+print('Two numbers are equal or not:',a==b)  
+print('Two numbers are not equal or not:',a!=b)  
+print('a is less than or equal to b:',a<=b)  
+print('a is greater than or equal to b:',a>=b)  
+print('a is greater b:',a>b)  
+print('a is less than b:',a<b)  
+
+# Now we give code examples of Assignment operators in Python. The code is given below -
+
+a = 32         # Initialize the value of a  
+b = 6          # Initialize the value of b  
+print('a=b:', a==b)  
+print('a+=b:', a+b)  
+print('a-=b:', a-b)  
+print('a*=b:', a*b)  
+print('a%=b:', a%b)  
+print('a**=b:', a**b)  
+print('a//=b:', a//b)  
