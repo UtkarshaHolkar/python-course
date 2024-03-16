78359679f2220091173916e16d4ffd12ae2f7fca
UtkashaHolakr <utkarshaaholkar@gmail.com>
2024-03-16 22:40:03 +0530

    first commit

---
+++ ifElse.py
@@ -0,0 +1,10 @@
+# # Indentation in Python
+# #For the ease of programming and to achieve simplicity, python doesn't allow the use of parentheses for the block level code. In Python, indentation is used to declare a block. If two statements are at the same indentation level, then they are the part of the same block.
+
+# Generally, four spaces are given to indent the statements which are a typical amount of indentation in python.
+
+# Indentation is the most used part of the python language since it declares the block of code. All the statements of one block are intended at the same level indentation. We will see how the actual indentation takes place in decision making and other stuff in python.
+
+# The if statement
+# The if statement is used to test a particular condition and if the condition is true, it executes a block of code known as if-block. The condition of if statement can be any valid logical expression which can be either evaluated to true or false.
+
